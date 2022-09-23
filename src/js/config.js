897390324d526c5cfa8e4a4c12eb7ebe2fdef034
bebe2003 <tranlethanhminh@gmail.com>
2022-09-23 09:27:13 +0900

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const KEY = '5c92fe39-de34-4771-8ca4-563e57e78fc6';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 12;
+export const MODAL_CLOSE_SEC = 2.5;
